63411783733f7ac9661649e07f2c2c7d4d9a0675
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2019-08-01 13:42:01 -0500

    Updates

---
+++ monaco-editor-worker-loader-proxy.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+self.MonacoEnvironment = {
+  baseUrl: "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.17.1/min/"
+};
+
+importScripts(
+  "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.17.1/min/vs/base/worker/workerMain.js"
+);
---
+++ precache-manifest.5ee1f61c6829c89a86c33c224a040d5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96c02ec1b8537a2ccf6f8f8b16dc5218",
+    "url": "/react-poc-sandbox/index.html"
+  },
+  {
+    "revision": "76ec3a3a6b9111fc9280",
+    "url": "/react-poc-sandbox/static/css/main.d96c2583.chunk.css"
+  },
+  {
+    "revision": "aedb5b00ca4d6799fd02",
+    "url": "/react-poc-sandbox/static/js/2.42f6eeba.chunk.js"
+  },
+  {
+    "revision": "76ec3a3a6b9111fc9280",
+    "url": "/react-poc-sandbox/static/js/main.f88a7dd6.chunk.js"
+  },
+  {
+    "revision": "e7f4a565425da239b2b6",
+    "url": "/react-poc-sandbox/static/js/runtime~main.bcfccd37.js"
+  }
+]);
